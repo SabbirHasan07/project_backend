1d8db0561e292d014ca3f909b4817d0ab7d830aa
SabbirHasan07 <sabbirhasan.engr@gmail.com>
2025-01-25 22:58:52 +0600

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors'); 
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
-
-
+app.use(cors());
 const pool = mysql.createPool({
   host: 'srv1667.hstgr.io',
   user: 'u474521097_project_user',
@@ -17,8 +17,7 @@ const pool = mysql.createPool({
 });
 
 
-app.use(cors());
-
+app.use(bodyParser.json());
 
 pool.getConnection((err, connection) => {
   if (err) {
@@ -27,6 +26,30 @@ pool.getConnection((err, connection) => {
   }
   console.log('Connected to the MySQL database');
   connection.release(); 
+});
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+
+  pool.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ message: 'Login successful', user: results[0] });
+    } else {
+      res.status(401).json({ message: 'Invalid username or password' });
+    }
+  });
 });
 
 
@@ -40,7 +63,6 @@ app.get('/data', (req, res) => {
     res.json(results);
   });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
